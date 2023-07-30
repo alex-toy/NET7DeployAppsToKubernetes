fd1be88f6c44f6aacb439c6de716bce1d872a582
alex-toy <alexei.80@hotmail.fr>
2023-07-30 10:40:19 +0200

    added docker file

--- KubernetesApp/FormulaOneAPI/Controllers/WeatherForecastController.cs
+++ KubernetesApp/FormulaOneAPI/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ namespace FormulaOneAPI.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = "hello friends from local app " + Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
